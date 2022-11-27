740e70acf79fc1af4c544b0a68292eb567d9a7ca
anhinga <anhinga@users.noreply.github.com>
2022-11-26 22:52:29 -0500

    function add_activation

--- dmm-port-from-clojure/draft-activations.jl
+++ dmm-port-from-clojure/draft-activations.jl
@@ -1,9 +1,13 @@
 activation_functions = Dict{String, Function}()
 
+function add_activation(f)
+    activation_functions[String(Symbol(f))] = f
+end
+
 function accum_add_args(all_inputs)
     all_outputs = Dict{String, Any}()
     all_outputs["result"] = add_v_values(all_inputs["accum"], all_inputs["delta"])
     all_outputs
 end
 
-activation_functions["accum_add_args"] = accum_add_args
+add_activation(accum_add_args)
